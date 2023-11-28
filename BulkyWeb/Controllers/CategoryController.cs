2503af90986fd6631fb6efa66ce45a979ec39b25
sarang2551 <sarang.nirwan@gmail.com>
2023-11-28 19:38:20 +0100

    Added Category page with creating and editing

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,65 @@
+﻿using BulkyWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db= db;
+        }
+        public IActionResult Index() // http:localhost:Port/Controller=Category/Action=Index
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList); // returns a html file from the Views folder. Program searches directory named Category
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "Display Order and Name cannot be the same");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+            
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            // if id is not null 
+            Category? categoryFromDb = _db.Categories.FirstOrDefault(u=>u.Id==id);
+            if(categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb); // passes a Category object that auto populates the values in the Edit table
+        }
+        [HttpPost]
+        public IActionResult Edit(int id)
+        {
+            return View(id);
+        }
+        public IActionResult Delete(int id)
+        {
+            return RedirectToAction("Index");
+        }
+ 
+    }
+}
---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [AllowNull]
+        [MaxLength(30)]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+        [AllowNull]
+        [Required]
+        [Range(0,100, ErrorMessage = "Display Order has to be between 0 and 100")]
+        [DisplayName("Display Order")]
+        public int DisplayOrder { get; set; }
+    }
+}
