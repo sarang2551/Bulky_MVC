ca32d4dc04584d35223f5aa688d9de00284059ff
sarang2551 <sarang.nirwan@gmail.com>
2023-11-30 17:11:54 +0100

    Added similar razor project

---
+++ BulkyWebRazor_Temp/Models/Category.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+
+namespace BulkyWebRazor_Temp.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [AllowNull]
+        [MaxLength(30)]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+        [AllowNull]
+        [Required]
+        [Range(0, 100, ErrorMessage = "Display Order has to be between 0 and 100")]
+        [DisplayName("Display Order")]
+        public int DisplayOrder { get; set; }
+    }
+}
